44ddc68393f74ed0d8d6791af6be03768548fcd6
junseopark0331 <junseopark0331@icloud.com>
2023-12-09 15:41:02 +0900

    :closed_lock_with_key: BaseURL Plist에서 가져올 수 있게 수정
    BaseURL Plist에서 가져올 수 있게 수정

--- Miso/Sources/Data/General/Miso+Bundle.swift
+++ Miso/Sources/Data/General/Miso+Bundle.swift
@@ -1,8 +1,19 @@
-//
-//  Miso+Bundle.swift
-//  Miso
-//
-//  Created by 박준서 on 12/6/23.
-//
-
 import Foundation
+
+extension Bundle {
+    var baseURL: String {
+        guard let file = self.path(forResource: "BaseURL", ofType: "plist") else {
+            fatalError("BaseURL.plist 파일을 찾을 수 없습니다.")
+        }
+        
+        guard let resource = NSDictionary(contentsOfFile: file) else {
+            fatalError("BaseURL.plist 파일 형식 에러")
+        }
+        
+        if let key = resource["Base-API-KEY"] as? String {
+            return key
+        } else {
+            fatalError("BaseURL.plist 파일에서 'Base-API-KEY' 값을 찾을 수 없습니다.")
+        }
+    }
+}
