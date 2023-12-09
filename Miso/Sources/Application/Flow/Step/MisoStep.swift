e1de118da668305e525c2df824e5194f08b32103
junseopark0331 <junseopark0331@icloud.com>
2023-12-09 15:35:49 +0900

    :sparkles: MisoStep 작성
     MisoStep 작성

--- Miso/Sources/Application/Flow/Step/MisoStep.swift
+++ Miso/Sources/Application/Flow/Step/MisoStep.swift
@@ -1,8 +1,39 @@
 //
-//  MisoStep.swift
-//  Miso
+//  GOMSStep.swift
+//  GOMS-iOS
 //
-//  Created by 박준서 on 12/8/23.
+//  Created by 선민재 on 2023/04/18.
 //
 
-import Foundation
+import RxFlow
+import UIKit
+
+enum MisoStep: Step {
+    
+    // MARK: - Auth
+    case loginInIsRequired
+    case signupIsRequired
+    case certificationNumberIsRequied
+    case mainVCIsRequired
+    case findPassword_phoneNumberAuth
+    
+    // MARK: TabBar
+    case tabBarIsRequired
+    
+    // MARK: Home
+    case searchIsRequired
+    case marketIsRequired
+    case requestIsRequired
+    case settingIsRequired
+    
+    case studentInfoIsRequired
+    case searchModalIsRequired
+    case searchModalDismiss
+    case editUserModalIsRequired(accountIdx: UUID)
+    case editModalDismiss
+    
+    //MARK: Alert
+    case alert(title: String?, message: String?, style: UIAlertController.Style, actions: [UIAlertAction])
+    case failureAlert(title: String?, message: String?, action: [UIAlertAction] = [])
+}
+
