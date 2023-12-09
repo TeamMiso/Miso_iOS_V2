fef05173cd9102d41879664dd4f54314aed61f0d
junseopark0331 <junseopark0331@icloud.com>
2023-12-09 15:44:49 +0900

    :lipstick: SplashVC 작성
    SplashVC 작성

--- Miso/Sources/Presentation/Splash/SplashVC.swift
+++ Miso/Sources/Presentation/Splash/SplashVC.swift
@@ -1,8 +1,30 @@
-//
-//  SplashVC.swift
-//  Miso
-//
-//  Created by 박준서 on 12/9/23.
-//
+import UIKit
+import SnapKit
+import Then
 
-import Foundation
+final class SplashVC: UIViewController {
+    
+    private let misoLogoImage = UIImageView().then {
+        $0.image = UIImage(named: "MisoLogo")
+    }
+    
+    func setLayout() {
+        misoLogoImage.snp.makeConstraints {
+            $0.height.width.equalTo(160)
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    func addviews(){
+        view.addSubview(
+            misoLogoImage
+        )
+    }
+    
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor(rgb: 0x25D07D)
+        addviews()
+        setLayout()
+    }
+    
+}
