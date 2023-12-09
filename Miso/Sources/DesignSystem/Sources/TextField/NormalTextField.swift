e56db17d08b32f69d34ef79f38aeb856557ae2f4
junseopark0331 <junseopark0331@icloud.com>
2023-12-09 15:42:55 +0900

    :lipstick: TextField UI 수정
     TextField UI 수정

--- Miso/Sources/DesignSystem/Sources/TextField/NormalTextField.swift
+++ Miso/Sources/DesignSystem/Sources/TextField/NormalTextField.swift
@@ -1,24 +1,24 @@
 import UIKit
 
-public final class NormalTextField: UITextField {
+public final class NormalTextField: UITextField{
+    
     init(placeholder: String) {
         super.init(frame: .zero)
         self.placeholder = placeholder
         setupView()
     }
-
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-
+    
     func setupView() {
-        backgroundColor = UIColor(rgb: 0xFAFAFA)
-        font = .miso(size: 13, family: .extraLight)
-        setPlaceholderColor(UIColor(rgb: 0x808080))
-
-        let lockImageView = UIImageView(image: UIImage(named: "Mail"))
-
-        leftView = lockImageView
-        leftViewMode = .unlessEditing
+        layer.cornerRadius = 8
+        layer.borderWidth = 1
+        layer.borderColor = UIColor(rgb: 0xBFBFBF).cgColor
+        backgroundColor = UIColor(rgb: 0xFFFFFF)
+        font = .miso(size: 14, family: .regular)
+        setPlaceholderColor(UIColor(rgb: 0xBFBFBF))
     }
+    
 }
--- Miso/Sources/DesignSystem/Sources/TextField/SecureTextField.swift
+++ Miso/Sources/DesignSystem/Sources/TextField/SecureTextField.swift
@@ -1,45 +1,50 @@
 import UIKit
 
 public final class SecureTextField: UITextField {
+    
     var passwordToggleButton = UIButton(type: .custom)
-
+    
     init(placeholder: String) {
         super.init(frame: .zero)
         self.placeholder = placeholder
         setupView()
     }
-
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-
+    
     func setupView() {
-        backgroundColor = UIColor(rgb: 0xFAFAFA)
-        font = .miso(size: 13, family: .extraLight)
-        setPlaceholderColor(UIColor(rgb: 0x808080))
-
+        layer.cornerRadius = 8
+        layer.borderWidth = 1
+        layer.borderColor = UIColor(rgb: 0xBFBFBF).cgColor
+        backgroundColor = UIColor(rgb: 0xFFFFFF)
+        font = .miso(size: 14, family: .regular)
+        setPlaceholderColor(UIColor(rgb: 0xBFBFBF))
+        
         isSecureTextEntry = true
         setPasswordToggleButtonImage()
     }
-
+    
     func setPasswordToggleButtonImage() {
-        passwordToggleButton = UIButton(primaryAction: UIAction(handler: { [self] _ in
+        passwordToggleButton = UIButton.init (primaryAction: UIAction (handler: { [self]_ in
             isSecureTextEntry.toggle()
-            self.passwordToggleButton.isSelected.toggle()
+            self.passwordToggleButton.isSelected.toggle ()
         }))
-
+        
         var buttonConfiguration = UIButton.Configuration.plain()
         buttonConfiguration.imagePadding = 10
         buttonConfiguration.baseBackgroundColor = .clear
-
-        passwordToggleButton.setImage(UIImage(systemName: "eye")?.withTintColor(UIColor(rgb: 0x999999), renderingMode: .alwaysOriginal), for: .normal)
-
+        
+        passwordToggleButton.setImage (UIImage (systemName: "eye")?.withTintColor(UIColor(rgb: 0x999999), renderingMode: .alwaysOriginal), for: .normal)
         passwordToggleButton.setImage(UIImage(systemName: "eye.slash")?.withTintColor(UIColor(rgb: 0x999999), renderingMode: .alwaysOriginal), for: .selected)
-
+        
         passwordToggleButton.configuration = buttonConfiguration
         passwordToggleButton.isSelected.toggle()
-
+        
         rightView = passwordToggleButton
         rightViewMode = .always
     }
 }
+
+
