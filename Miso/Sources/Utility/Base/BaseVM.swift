e33d1e610ec0c803f18e7a4230e650ca4a16e9ab
junseopark0331 <junseopark0331@icloud.com>
2023-12-09 15:50:20 +0900

    Base 코드들

--- Miso/Sources/Utility/Base/BaseVM.swift
+++ Miso/Sources/Utility/Base/BaseVM.swift
@@ -8,6 +8,7 @@ class BaseVM {
     var disposeBag = DisposeBag()
     var steps = PublishRelay<Step>()
     let keychain = Keychain()
-//    let gomsRefreshToken = GOMSRefreshToken.shared
-//    lazy var accessToken = "Bearer " + (keychain.read(key: Const.KeychainKey.accessToken) ?? "")
+//    let refreshToken = GOMSRefreshToken.shared
+//    lazy var accessToken = try keychain.load(type: .accessToken)
+
 }
