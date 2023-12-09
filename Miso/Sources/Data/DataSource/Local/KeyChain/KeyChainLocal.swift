f7588606edc429b24ce958ea19acaf8cd9df235d
junseopark0331 <junseopark0331@icloud.com>
2023-12-09 15:39:42 +0900

    :sparkles: KeyChainLocal 작성
    KeyChainLocal 작성

--- Miso/Sources/Data/DataSource/Local/KeyChain/KeyChainLocal.swift
+++ Miso/Sources/Data/DataSource/Local/KeyChain/KeyChainLocal.swift
@@ -1,8 +1,56 @@
-//
-//  KeyChainLocal.swift
-//  Miso
-//
-//  Created by 박준서 on 12/7/23.
-//
-
 import Foundation
+
+final class KeychainLocal {
+    static let shared = KeychainLocal()
+    private let keychain = Keychain()
+    private init() {}
+
+    func saveAccessToken(_ token: String) {
+        keychain.save(type: .accessToken, value: token)
+    }
+
+    func fetchAccessToken() throws -> String {
+        return try keychain.load(type: .accessToken)
+    }
+
+    func deleteAccessToken() {
+        keychain.delete(type: .accessToken)
+    }
+
+    func saveRefreshToken(_ token: String) {
+        keychain.save(type: .refreshToken, value: token)
+    }
+
+    func fetchRefreshToken() throws -> String {
+        return try keychain.load(type: .refreshToken)
+    }
+
+    func deleteRefreshToken() {
+        keychain.delete(type: .refreshToken)
+    }
+
+    func saveAccessExp(_ date: String) {
+        keychain.save(type: .accessExpiredAt, value: date)
+    }
+
+    func fetchAccessExp() throws -> String {
+        return try keychain.load(type: .accessExpiredAt)
+    }
+
+    func deleteAccessExp() {
+        keychain.delete(type: .accessExpiredAt)
+    }
+
+    func saveRefreshExp(_ date: String) {
+        keychain.save(type: .refreshExpiredAt, value: date)
+    }
+
+    func fetchRefreshExp() throws -> String {
+        return try keychain.load(type: .refreshExpiredAt)
+    }
+
+    func deleteRefreshExp() {
+        keychain.delete(type: .refreshExpiredAt)
+    }
+}
+
