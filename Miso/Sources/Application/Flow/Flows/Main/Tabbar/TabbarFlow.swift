4cd41b4d8698194b33a1a277e4803a709d84281c
junseopark0331 <junseopark0331@icloud.com>
2023-12-10 22:16:36 +0900

    :sparkles: TabbarFlow 작성
    TabbarFlow 작성

--- Miso/Sources/Application/Flow/Flows/Main/Tabbar/TabbarFlow.swift
+++ Miso/Sources/Application/Flow/Flows/Main/Tabbar/TabbarFlow.swift
@@ -1,79 +1,119 @@
-//import RxFlow
-//import UIKit
-//
-//final class TabBarFlow: Flow {
-//    
-//    enum TabIndex: Int {
-//        case home = 0
-//        case qrCode = 1
-//        case outing = 2
-//    }
-//    
-//    var root: Presentable {
-//        return self.rootVC
-//    }
-//    
-//    private let rootVC = GOMSTabBarViewController()
-//    
-//    private var homeFlow = HomeFlow()
-//    private var qrCodeFlow = QRCodeFlow()
-//    private var outingFlow = OutingFlow()
-//    private let keychain = Keychain()
-//    private lazy var userAuthority = keychain.read(key: Const.KeychainKey.authority)
-//    
-//    init() {}
-//    
-//    func navigate(to step: Step) -> FlowContributors {
-//        guard let step = step as? GOMSStep else {return .none}
-//        
-//        switch step {
-//        case .tabBarIsRequired:
-//            return coordinateToTabbar()
-//            
-//        case .homeIsRequired:
-//            rootVC.selectedIndex = 0
-//            return .none
-//            
-//        case .qrocdeIsRequired:
-//            rootVC.selectedIndex = 1
-//            return .none
-//            
-//        case .outingIsRequired:
-//            rootVC.selectedIndex = 2
-//            return .none
-//            
-//        case .introIsRequired:
-//            return .end(forwardToParentFlowWithStep: GOMSStep.introIsRequired)
-//            
-//        default:
-//            return .none
-//        }
-//    }
-//    
-//}
-//
-//private extension TabBarFlow {
-//    func coordinateToTabbar() -> FlowContributors {
-//        Flows.use(
-//            homeFlow, qrCodeFlow, outingFlow,
-//            when: .ready
-//        ) { [unowned self] (root1: UINavigationController,
-//                            root2: UINavigationController,
-//                            root3: UINavigationController) in
-//            lazy var userIsOuting = UserDefaults.standard.bool(forKey: "userIsOuting")
-//            
-//            
-//            root1.tabBarItem = homeItem
-//            root2.tabBarItem = qrCodeItem
-//            root3.tabBarItem = outingItem
-//            
-//            self.rootVC.setViewControllers([root1,root2,root3], animated: true)
-//        }
-//        return .multiple(flowContributors: [
-//            .contribute(withNextPresentable: homeFlow, withNextStepper: OneStepper(withSingleStep: GOMSStep.homeIsRequired)),
-//            .contribute(withNextPresentable: qrCodeFlow, withNextStepper: OneStepper(withSingleStep: GOMSStep.qrocdeIsRequired)),
-//            .contribute(withNextPresentable: outingFlow, withNextStepper: OneStepper(withSingleStep: GOMSStep.outingIsRequired))
-//        ])
-//    }
-//}
-//
+import RxFlow
+import UIKit
+
+final class TabBarFlow: Flow {
+    
+    enum TabIndex: Int {
+        case home = 0
+        case qrCode = 1
+        case outing = 2
+    }
+    
+    var root: Presentable {
+        return self.rootVC
+    }
+    
+    private let rootVC = MisoTabBarVC()
+    
+    private var searchFlow = SearchFlow()
+    private var marketFlow = SearchFlow()
+    private var cameraFlow = SearchFlow()
+    private var inquiryFlow = SearchFlow()
+    private var settingFlow = SearchFlow()
+    
+    init() {}
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? MisoStep else {return .none}
+        
+        switch step {
+        case .tabBarIsRequired:
+            return coordinateToTabbar()
+            
+        case .searchTabbarIsRequired:
+            rootVC.selectedIndex = 0
+            return .none
+            
+        case .marketTabbarIsRequired:
+            rootVC.selectedIndex = 1
+            return .none
+            
+        case .cameraTabbarIsRequired:
+            rootVC.selectedIndex = 2
+            return .none
+            
+        case .inquiryTabbarIsRequired:
+            rootVC.selectedIndex = 3
+            return .none
+            
+        case .settingTabbarIsRequired:
+            rootVC.selectedIndex = 4
+            return .none
+            
+        case .loginVCIsRequired:
+            return .end(forwardToParentFlowWithStep: MisoStep.loginVCIsRequired)
+            
+        default:
+            return .none
+        }
+    }
+    
+}
+
+private extension TabBarFlow {
+    func coordinateToTabbar() -> FlowContributors {
+        Flows.use(
+            searchFlow, marketFlow, cameraFlow, inquiryFlow, settingFlow,
+            when: .ready
+        ) { [unowned self] ( 
+            root1: UINavigationController,
+            root2: UINavigationController,
+            root3: UINavigationController,
+            root4: UINavigationController,
+            root5: UINavigationController) in
+
+            let searchItem = UITabBarItem(
+                title: "검색",
+                image: UIImage(named: "Search"),
+                selectedImage: UIImage(named: "Search.selected")
+            )
+            let marketItem = UITabBarItem(
+                title: "상점",
+                image: UIImage(named: "Market"),
+                selectedImage: UIImage(named: "Market.selected")
+            )
+            let cameraItem = UITabBarItem(
+                title: "카메라",
+                image: UIImage(named: "Camera"),
+                selectedImage: UIImage(named: "Camera")
+            )
+            let inquiryItem = UITabBarItem(
+                title: "홈",
+                image: UIImage(named: "Inquiry"),
+                selectedImage: UIImage(named: "Inquiry.selected")
+            )
+            let settingItem = UITabBarItem(
+                title: "홈",
+                image: UIImage(named: "Setting"),
+                selectedImage: UIImage(named: "Setting.selected")
+            )
+            
+        
+            root1.tabBarItem = searchItem
+            root2.tabBarItem = marketItem
+            root3.tabBarItem = cameraItem
+            root4.tabBarItem = inquiryItem
+            root5.tabBarItem = settingItem
+            
+            self.rootVC.setViewControllers([root1,root2,root3,root4,root5], animated: true)
+        }
+        return .multiple(flowContributors: [
+            .contribute(withNextPresentable: searchFlow, withNextStepper: OneStepper(withSingleStep: MisoStep.searchTabbarIsRequired)),
+            .contribute(withNextPresentable: marketFlow, withNextStepper: OneStepper(withSingleStep: MisoStep.marketTabbarIsRequired)),
+            .contribute(withNextPresentable: cameraFlow, withNextStepper: OneStepper(withSingleStep: MisoStep.cameraTabbarIsRequired)),
+            .contribute(withNextPresentable: inquiryFlow, withNextStepper: OneStepper(withSingleStep: MisoStep.inquiryTabbarIsRequired)),
+            .contribute(withNextPresentable: settingFlow, withNextStepper: OneStepper(withSingleStep: MisoStep.settingTabbarIsRequired))
+        ])
+    }
+}
+
