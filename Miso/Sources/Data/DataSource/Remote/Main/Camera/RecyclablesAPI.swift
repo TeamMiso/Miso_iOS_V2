1f87bfb3a072c42fe999f104e068f56a5855896b
junseopark0331 <junseopark0331@icloud.com>
2023-12-15 00:05:20 +0900

    :sparkles: Recycle path 에 관한 API 작성
     Recycle path 에 관한 API 작성

--- Miso/Sources/Data/DataSource/Remote/Main/Camera/RecyclablesAPI.swift
+++ Miso/Sources/Data/DataSource/Remote/Main/Camera/RecyclablesAPI.swift
@@ -2,37 +2,58 @@ import Foundation
 import Moya
 
 enum RecyclablesAPI {
-    case itemList(accessToken: String)
-    case itemDetailList(id: String, accessToken: String)
+    case getDetailRecyclables(accessToken: String)
+    case searchRecyclables(accessToken: String)
+    case getAllRecyclables(accessToken: String)
+    case uploadImage(accessToken: String, image: Data)
 }
+
 extension RecyclablesAPI: TargetType {
-    
-    public var baseURL: URL {
-        return URL(string: BaseURL.baseURL)!
+        
+    var baseURL: URL {
+        return URL(string: BaseURL.baseURL + "/recyclables")!
     }
     
     var path: String {
         switch self {
-        case .itemList:
-            return "/item"
-        case let .itemDetailList(id, accessToken):
-            return "/item/\(id)"
+        case .getDetailRecyclables:
+            return ""
+        case .searchRecyclables:
+            return "/search"
+        case .getAllRecyclables:
+            return "/all"
+        case .uploadImage:
+            return "/process_image"
         }
     }
     
     var method: Moya.Method {
         switch self {
-        case .itemList, .itemDetailList:
+        case .getDetailRecyclables, .searchRecyclables, .getAllRecyclables:
             return .get
+        case .uploadImage:
+            return .post
         }
     }
     
     var sampleData: Data {
-        return "@@".data(using: .utf8)!
+        switch self {
+        case .getDetailRecyclables, .searchRecyclables, .getAllRecyclables:
+            return "@@".data(using: .utf8)!
+        case .uploadImage:
+            return Data()
+        }
     }
     
     var task: Task {
         switch self {
+        case let .uploadImage(acccessToken, image):
+            let multipartFormData = MultipartFormData(
+                provider: .data(image),
+                name: "recyclables",
+                fileName: "recyclable.jpg",
+                mimeType: "image/jpeg")
+            return .uploadMultipart([multipartFormData])
         default:
             return .requestPlain
         }
@@ -40,13 +61,13 @@ extension RecyclablesAPI: TargetType {
     
     var headers: [String : String]? {
         switch self {
-        case let .itemList(accessToken):
-            return ["Authorization": accessToken]
-        case let .itemDetailList(id, accessToken):
-            return ["Authorization": accessToken]
+        case let .uploadImage(accessToken, image):
+            return [
+                "Authorization": accessToken,
+                "Content-Type": "multipart/form-data"
+            ]
         default:
             return ["Content-Type": "application/json"]
         }
     }
 }
-
