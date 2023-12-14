22dd52d0e0a0bbf82269d0cb03edc786e87158c6
junseopark0331 <junseopark0331@icloud.com>
2023-12-15 01:33:19 +0900

    :recycle: BaseVM 작성
    BaseVM 작성

--- Miso/Sources/Utility/Base/BaseVM.swift
+++ Miso/Sources/Utility/Base/BaseVM.swift
@@ -1,14 +1,21 @@
-import Moya
+import SwiftUI
+import RxSwift
 import RxCocoa
 import RxFlow
-import RxSwift
-import UIKit
+import Moya
 
-class BaseVM {
+class BaseVM: ObservableObject { // 수정: ObservableObject 채택
     var disposeBag = DisposeBag()
     var steps = PublishRelay<Step>()
     let keychain = Keychain()
-//    let refreshToken = GOMSRefreshToken.shared
-//    lazy var accessToken = try keychain.load(type: .accessToken)
-
+    let misoRefreshToken = MisoRefreshToken.shared
+    
+    lazy var accessToken: String = {
+        do {
+            return "Bearer " + (try keychain.load(type: .accessToken) ?? "")
+        } catch {
+            print("Refresh 토큰을 불러오는 중 에러 발생: \(error)")
+            return "Bearer "
+        }
+    }()
 }
