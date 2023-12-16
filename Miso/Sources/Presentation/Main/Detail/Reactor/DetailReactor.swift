03b6c66c9268dc4b39221db7e206c440f552bf5f
junseopark0331 <junseopark0331@icloud.com>
2023-12-16 23:45:09 +0900

    :sparkles: 포인트 받기 버튼이 클릭되었을때 searchFlow로 가능 기능 구현
    포인트 받기 버튼이 클릭되었을때 searchFlow로

--- Miso/Sources/Presentation/Main/Detail/Reactor/DetailReactor.swift
+++ Miso/Sources/Presentation/Main/Detail/Reactor/DetailReactor.swift
@@ -4,16 +4,32 @@ import ReactorKit
 import RxCocoa
 import RxFlow
 import RxSwift
+import UIKit
 
 class DetailReactor: Reactor, Stepper {
     
     var disposeBag = DisposeBag()
     var steps = PublishRelay<Step>()
     var initialState: State
+    let userProvider = MoyaProvider<UserAPI>(plugins: [NetworkLoggerPlugin()])
+    
+    let keychain = Keychain()
+    let misoRefreshToken = MisoRefreshToken.shared
+    lazy var accessToken: String = {
+        do {
+            return "Bearer " + (try keychain.load(type: .accessToken))
+        } catch {
+            print("Refresh 토큰을 불러오는 중 에러 발생: \(error)")
+            return "Bearer "
+        }
+    }()
+    
     var uploadRecyclablesList: UploadRecyclablesListResponse?
+    var originalImage: UIImage?
+    
     
     enum Action {
-        
+        case pointButtonTapped
     }
     
     enum Mutation {
@@ -22,21 +38,23 @@ class DetailReactor: Reactor, Stepper {
     
     struct State {
         var uploadRecyclablesList: UploadRecyclablesListResponse?
+        var originalImage: UIImage?
         
-        init(uploadRecyclablesList: UploadRecyclablesListResponse?) {
+        init(uploadRecyclablesList: UploadRecyclablesListResponse?, originalImage: UIImage?) {
             self.uploadRecyclablesList = uploadRecyclablesList
+            self.originalImage = originalImage
         }
     }
     
     
-    init(uploadRecyclablesList: UploadRecyclablesListResponse? = nil) {
-        self.initialState = State(uploadRecyclablesList: uploadRecyclablesList)
+    init(uploadRecyclablesList: UploadRecyclablesListResponse? = nil, originalImage: UIImage? = nil) {
+        self.initialState = State(uploadRecyclablesList: uploadRecyclablesList, originalImage: originalImage)
     }
-    
+}
+
+extension DetailReactor {
     func mutate(action: Action) -> Observable<Mutation> {
-        switch action {
-            
-        }
+        return pointButtonTapped()
     }
     
     func reduce(state: State, mutation: Mutation) -> State {
@@ -45,5 +63,33 @@ class DetailReactor: Reactor, Stepper {
             
         }
         return newState
+    }
+}
+
+private extension DetailReactor {
+    private func pointButtonTapped() -> Observable<Mutation>  {
+        userProvider.request(.giveUserPoint(accessToken: accessToken)) { response in
+            switch response {
+            case .success(let result):
+                let statusCode = result.statusCode
+                
+                switch statusCode{
+                case 200:
+                    print("point 주기")
+                    self.steps.accept(MisoStep.coordinateToSearchVCIsRequired)
+                case 401:
+                    print("토큰이 유효하지 않습니다.")
+                case 404:
+                    print("사용자를 찾을 수 없습니다.")
+                case 500:
+                    print("서버 오류")
+                default:
+                    print(statusCode)
+                }
+            case .failure(let err):
+                print(String(describing: err))
+            }
+        }
+        return .empty()
     }
 }
