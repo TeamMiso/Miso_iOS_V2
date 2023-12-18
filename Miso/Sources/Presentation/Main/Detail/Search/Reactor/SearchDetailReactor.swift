eb12999028ce9a7dc73a6be3682195ea77f88d54
junseopark0331 <junseopark0331@icloud.com>
2023-12-18 14:44:48 +0900

    :sparkles: 검색했을때 나오는 화면
    검색했을때 나오는 화면

--- Miso/Sources/Presentation/Main/Detail/Search/Reactor/SearchDetailReactor.swift
+++ Miso/Sources/Presentation/Main/Detail/Search/Reactor/SearchDetailReactor.swift
@@ -1,8 +1,61 @@
-//
-//  SearchDetailReactor.swift
-//  Miso
-//
-//  Created by 박준서 on 12/18/23.
-//
-
 import Foundation
+import Moya
+import ReactorKit
+import RxCocoa
+import RxFlow
+import RxSwift
+import UIKit
+
+class SearchDetailReactor: Reactor, Stepper {
+    
+    var disposeBag = DisposeBag()
+    var steps = PublishRelay<Step>()
+    var initialState: State
+    let userProvider = MoyaProvider<UserAPI>(plugins: [NetworkLoggerPlugin()])
+    
+    let keychain = Keychain()
+    let misoRefreshToken = MisoRefreshToken.shared
+    lazy var accessToken: String = {
+        do {
+            return "Bearer " + (try keychain.load(type: .accessToken))
+        } catch {
+            print("Refresh 토큰을 불러오는 중 에러 발생: \(error)")
+            return "Bearer "
+        }
+    }()
+    
+    var detailRecyclablesList: DetailRecyclablesListResponse?
+
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        var detailRecyclablesList: DetailRecyclablesListResponse?
+        
+        init(detailRecyclablesList: DetailRecyclablesListResponse?) {
+            self.detailRecyclablesList = detailRecyclablesList
+        }
+    }
+    
+    
+    init(detailRecyclablesList: DetailRecyclablesListResponse? = nil) {
+        self.initialState = State(detailRecyclablesList: detailRecyclablesList)
+    }
+}
+
+extension SearchDetailReactor {
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+            
+        }
+        return newState
+    }
+}
+
