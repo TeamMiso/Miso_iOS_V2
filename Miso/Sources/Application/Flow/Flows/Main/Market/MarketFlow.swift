fe4f33dba9d4115e480322af2b140abac7ac078d
junseopark0331 <junseopark0331@icloud.com>
2023-12-19 23:08:52 +0900

    :sparkles: MarketFlow 작성
     MarketFlow 작성

--- Miso/Sources/Application/Flow/Flows/Main/Market/MarketFlow.swift
+++ Miso/Sources/Application/Flow/Flows/Main/Market/MarketFlow.swift
@@ -1,8 +1,56 @@
-//
-//  MarketFlow.swift
-//  Miso
-//
-//  Created by 박준서 on 12/18/23.
-//
+import RxFlow
+import UIKit
+import RxSwift
+import RxCocoa
 
-import Foundation
+struct MarketStepper: Stepper{
+    var steps = PublishRelay<Step>()
+
+    var initialStep: Step{
+        return MisoStep.marketTabbarIsRequired
+    }
+}
+
+class MarketFlow: Flow {
+    
+    var root: Presentable {
+        return self.rootViewController
+    }
+    
+    var stepper = MarketStepper()
+    
+    private lazy var rootViewController = UINavigationController()
+    
+    init(){}
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? MisoStep else { return .none }
+        switch step {
+            
+        case .marketTabbarIsRequired:
+            return coordinateToMarketTabbar()
+            
+        default:
+            return .none
+        }
+    }
+    
+}
+
+private extension MarketFlow {
+    
+    private func coordinateToMarketTabbar() -> FlowContributors {
+        let reactor = MarketReactor()
+        let vc = MarketVC(reactor: reactor)
+        self.rootViewController.pushViewController(vc, animated: true)
+        return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: reactor))
+    }
+    
+    private func coordinateToSearchVC() -> FlowContributors {
+        print("coordinate")
+        let reactor = SearchReactor()
+        let vc = SearchVC(reactor: reactor)
+        self.rootViewController.popToRootViewController(animated: true)
+        return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: reactor))
+    }
+}
