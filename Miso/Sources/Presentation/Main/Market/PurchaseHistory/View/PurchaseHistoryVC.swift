78aaedd4ac844700c90defdb2d39118bbf7a54b9
junseopark0331 <junseopark0331@icloud.com>
2023-12-20 20:07:08 +0900

    💄PurchaseHistoryVC 작성

--- Miso/Sources/Presentation/Main/Market/PurchaseHistory/View/PurchaseHistoryVC.swift
+++ Miso/Sources/Presentation/Main/Market/PurchaseHistory/View/PurchaseHistoryVC.swift
@@ -4,17 +4,16 @@ import RxSwift
 import RxCocoa
 import RxDataSources
 
-
-final class PurchaseHistoryVC: BaseVC<MarketReactor> {
+final class PurchaseHistoryVC: BaseVC<PurchaseHistoryReactor> {
     
     let purchaseHisoryListFlowLayout = UICollectionViewFlowLayout().then {
         $0.scrollDirection = .vertical
-        $0.minimumLineSpacing = 16
+        $0.minimumLineSpacing = 8
         $0.itemSize = CGSize(width: (UIScreen.main.bounds.width - 32) , height: 80)
     }
     
     lazy var purchaseHisoryListCollectionView = UICollectionView(frame: .zero, collectionViewLayout: purchaseHisoryListFlowLayout).then {
-        $0.register(ItemListCell.self, forCellWithReuseIdentifier: ItemListCell.identifier)
+        $0.register(PurchaseHistoryCell.self, forCellWithReuseIdentifier: PurchaseHistoryCell.identifier)
         $0.backgroundColor = .white
     }
     
@@ -32,31 +31,30 @@ final class PurchaseHistoryVC: BaseVC<MarketReactor> {
         }
     }
     
-    override func bindAction(reactor: MarketReactor) {
+    override func bindAction(reactor: PurchaseHistoryReactor) {
         self.rx.methodInvoked(#selector(viewDidLoad))
-            .map { _ in MarketReactor.Action.fetchItemList }
+            .map { _ in PurchaseHistoryReactor.Action.fetchPurchaseHistoryList }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
     }
     
-    override func bindState(reactor: MarketReactor) {
+    override func bindState(reactor: PurchaseHistoryReactor) {
         reactor.state
-            .map{ $0.itemListArray}
+            .map{ $0.purchaseHistoryResponse}
             .bind(to: purchaseHisoryListCollectionView.rx.items(
-                cellIdentifier: "PurchaseHistoryCell", cellType: ItemListCell.self)
+                cellIdentifier: PurchaseHistoryCell.identifier, cellType: PurchaseHistoryCell.self)
             ) {
                 index, response, cell in
-                cell.itemImage.kf.setImage(with: URL(string: response.imageUrl))
+                cell.itemImageView.kf.setImage(with: URL(string: response.imageUrl))
+                cell.purchaseDateLabel.text = response.createdDate
                 cell.itemNameLabel.text = response.name
-                cell.itemPointLabel.text = "\(response.price)"
-                
             }
             .disposed(by: disposeBag)
     }
     
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         self.navigationController?.navigationBar.prefersLargeTitles = false
     }
 }
-
