07de96554848eaca2237104fdd7ab7eeffccf0a3
junseopark0331 <junseopark0331@icloud.com>
2023-12-20 19:49:38 +0900

    :sparkles: 엔드포인트가 끝나는 /purchase 로 끝나는 response 생성
    엔드포인트가 끝나는 /purchase 로

--- Miso/Sources/Data/DataSource/Remote/Response/Main/Purchase/PurchaseResponse.swift
+++ Miso/Sources/Data/DataSource/Remote/Response/Main/Purchase/PurchaseResponse.swift
@@ -1,8 +1,14 @@
-//
-//  PurchaseResponse.swift
-//  Miso
-//
-//  Created by 박준서 on 12/20/23.
-//
-
 import Foundation
+
+struct PurchaseHistoryResponse: Decodable {
+    
+    let purchaseList: [PurchaseList]
+    
+    struct PurchaseList: Decodable {
+        let id: Int
+        let price: Int
+        let name: String
+        let imageUrl: String
+        let createdDate: String
+    }
+}
