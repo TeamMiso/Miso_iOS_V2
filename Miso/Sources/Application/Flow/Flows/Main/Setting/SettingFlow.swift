ef6eff7512bffc288e64e891060dbd6aa5b62d36
junseopark0331 <junseopark0331@icloud.com>
2023-12-21 22:25:39 +0900

    :sparkles: Settingflow 작성

--- Miso/Sources/Application/Flow/Flows/Main/Setting/SettingFlow.swift
+++ Miso/Sources/Application/Flow/Flows/Main/Setting/SettingFlow.swift
@@ -3,21 +3,21 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-struct SettiingStepper: Stepper{
+struct SettingStepper: Stepper{
     var steps = PublishRelay<Step>()
 
     var initialStep: Step{
-        return MisoStep.inquiryTabbarIsRequired
+        return MisoStep.settingTabbarIsRequired
     }
 }
 
-class SettiingFlow: Flow {
+class SettingFlow: Flow {
     
     var root: Presentable {
         return self.rootViewController
     }
     
-    var stepper = SettiingStepper()
+    var stepper = SettingStepper()
     
     private lazy var rootViewController = UINavigationController()
     
@@ -26,14 +26,8 @@ class SettiingFlow: Flow {
     func navigate(to step: Step) -> FlowContributors {
         guard let step = step as? MisoStep else { return .none }
         switch step {
-        case .inquiryTabbarIsRequired:
-            return coordinateToInquiryTabbar()
-        case let .detailInquiryVCIsRequired(data):
-            return coordinateToInquiryDetailVC(data: data)
-        case .writeInquiryVCIsRequired:
-            return coordinateToWriteInquiryVC()
-        case let .alert(title ,message, style, actions):
-            return presentToAlert(title: title, message: message, style: style, actions: actions)
+        case .settingTabbarIsRequired:
+            return coordinateTosettingTabbar()
         default:
             return .none
         }
@@ -41,34 +35,12 @@ class SettiingFlow: Flow {
     
 }
 
-private extension SettiingFlow {
-    
-    private func coordinateToInquiryTabbar() -> FlowContributors {
-        let reactor = InquiryReactor()
-        let vc = InquiryVC(reactor: reactor)
+private extension SettingFlow {
+    private func coordinateTosettingTabbar() -> FlowContributors {
+        let reactor = SettingReactor()
+        let vc = SettingVC(reactor: reactor)
         self.rootViewController.pushViewController(vc, animated: true)
         return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: reactor))
     }
     
-    private func coordinateToInquiryDetailVC(data: DetailInquiryResponse) -> FlowContributors {
-        let reactor = DetailInquiryReactor(detailInquiryResponse: data)
-        let vc = DetailInquiryVC(reactor: reactor)
-        self.rootViewController.pushViewController(vc, animated: true)
-        return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: reactor))
-    }
-    
-    private func coordinateToWriteInquiryVC() -> FlowContributors {
-        let reactor = WriteInquiryReactor()
-        let vc = WriteInquiryVC(reactor: reactor)
-        self.rootViewController.pushViewController(vc, animated: true)
-        return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: reactor))
-    }
-    
-    func presentToAlert(title: String?, message: String?, style: UIAlertController.Style, actions: [UIAlertAction]) -> FlowContributors {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
-        actions.forEach { alert.addAction($0) }
-        self.rootViewController.topViewController?.present(alert, animated: true)
-        return .none
-    }
 }
-
