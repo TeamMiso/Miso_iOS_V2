1cdb578edc336a9601c994ba3238536a554c527a
junseopark0331 <junseopark0331@icloud.com>
2023-12-21 23:14:14 +0900

    :lipstick: Point 추가

--- Miso/Sources/Presentation/Main/Detail/Item/View/ItemDetailVC.swift
+++ Miso/Sources/Presentation/Main/Detail/Item/View/ItemDetailVC.swift
@@ -24,11 +24,19 @@ final class ItemDetailVC: BaseVC<ItemDetailReactor> {
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
     
+    private let currentPointButton = UIButton().then {
+        $0.setTitle("1500 포인트", for: .normal)
+        $0.titleLabel?.font = .miso(size: 12, family: .semiBold)
+        $0.setTitleColor(UIColor(rgb: 0xBFBFBF), for: .normal)
+        $0.backgroundColor = UIColor(rgb: 0xFFFFFF)
+    }
+    
     override func addView() {
         view.addSubviews(
             itemImageView,
             contentLabel,
-            buyButton
+            buyButton,
+            currentPointButton
         )
     }
     
@@ -46,6 +54,11 @@ final class ItemDetailVC: BaseVC<ItemDetailReactor> {
         buyButton.snp.makeConstraints {
             $0.height.equalTo((bound.height)/17.75)
             $0.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(32)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+        currentPointButton.snp.makeConstraints {
+            $0.height.equalTo(20)
+            $0.top.equalTo(buyButton.snp.bottom).offset(4)
             $0.leading.trailing.equalToSuperview().inset(16)
         }
     }
