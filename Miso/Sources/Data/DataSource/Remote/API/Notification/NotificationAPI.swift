ed2b4cce8a269cf9258fac1f2f337632340b0579
junseopark0331 <junseopark0331@icloud.com>
2023-12-30 23:51:51 +0900

    ✨::[#15] NotificationAPI 추가

--- Miso/Sources/Data/DataSource/Remote/API/Notification/NotificationAPI.swift
+++ Miso/Sources/Data/DataSource/Remote/API/Notification/NotificationAPI.swift
@@ -1,8 +1,54 @@
-//
-//  NotificationAPI.swift
-//  Miso
-//
-//  Created by 박준서 on 12/30/23.
-//
-
 import Foundation
+import Moya
+
+enum NotificationAPI {
+    case saveDeviceToken(accessToken: String, deviceToken: String)
+    case getItemDetailList(accessToken: String, id: String)
+}
+extension NotificationAPI: TargetType {
+    
+    public var baseURL: URL {
+        return URL(string: BaseURL.baseURL + "/notification")!
+    }
+    
+    var path: String {
+        switch self {
+        case let .saveDeviceToken( _, deviceToken):
+            return "/save/\(deviceToken)"
+        case let .getItemDetailList( _, id):
+            return "/\(id)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .saveDeviceToken:
+            return .post
+        case .getItemDetailList:
+            return .get
+        }
+    }
+    
+    var sampleData: Data {
+        return "@@".data(using: .utf8)!
+    }
+    
+    var task: Task {
+        switch self {
+        default:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case let .saveDeviceToken(accessToken, _):
+            return ["Authorization": accessToken]
+        case let .getItemDetailList(accessToken, _):
+            return ["Authorization": accessToken]
+        default:
+            return ["Content-Type": "application/json"]
+        }
+    }
+}
+
