062d3d251fa95fff3a8893d763c4e7f019486dbb
junseopark0331 <junseopark0331@icloud.com>
2024-01-02 00:25:07 +0900

    ✨::[#19] 로그아웃 기능 추가

--- Miso/Sources/Presentation/Main/Setting/Reactor/SettingReactor.swift
+++ Miso/Sources/Presentation/Main/Setting/Reactor/SettingReactor.swift
@@ -13,6 +13,7 @@ class SettingReactor: Reactor, Stepper {
     var steps = PublishRelay<Step>()
     var initialState: State
     let userProvider = MoyaProvider<UserAPI>(plugins: [NetworkLoggerPlugin()])
+    let authProvider = MoyaProvider<AuthAPI>(plugins: [NetworkLoggerPlugin()])
     let keychain = Keychain()
     let misoRefreshToken = MisoRefreshToken.shared
     lazy var accessToken: String = {
@@ -28,6 +29,7 @@ class SettingReactor: Reactor, Stepper {
     
     enum Action {
         case fetchUserInfoResponse
+        case logoutButtonDidTap
     }
     
     enum Mutation {
@@ -48,6 +50,8 @@ extension SettingReactor {
         switch action {
         case .fetchUserInfoResponse:
             return fetchInquiryList()
+        case .logoutButtonDidTap:
+            return logout()
         }
     }
     
@@ -64,7 +68,7 @@ extension SettingReactor {
 // MARK: - Method
 private extension SettingReactor {
     
-    func fetchInquiryList() -> Observable<Mutation> {
+    private func fetchInquiryList() -> Observable<Mutation> {
         return Observable.create { observer in
             self.userProvider.request(.getUserInfo(accessToken: self.accessToken)){ response in
                 switch response {
@@ -92,5 +96,45 @@ private extension SettingReactor {
             }
             return Disposables.create()
         }
+    }
+    
+    private func logout() -> Observable<Mutation> {
+        self.steps.accept(MisoStep.alert(
+            title: "로그아웃",
+            message: "로그아웃하시겠습니까?",
+            style: .alert,
+            actions: [
+                UIAlertAction(title: "취소", style: .cancel),
+                UIAlertAction(title: "확인", style: .default) { (_) in
+                    self.authProvider.request(.logout(accessToken: self.accessToken)) { response in
+                        switch response {
+                        case .success(let result):
+                            let statusCode = result.statusCode
+                            switch statusCode{
+                            case 204:
+                                self.steps.accept(MisoStep.loginVCIsRequired)
+                                self.deleteUserToken()
+                            case 401:
+                                print("토큰이 유효하지 않습니다.")
+                            case 404:
+                                print("사용자를 찾을 수 없습니다.")
+                            case 500:
+                                print("서버 오류")
+                            default:
+                                print(statusCode)
+                            }
+                        case .failure(let err):
+                            print(String(describing: err))
+                        }
+                    }
+                }
+            ])
+        )
+        return .empty()
+    }
+    
+    private func deleteUserToken() {
+        keychain.delete(type: .accessToken)
+        keychain.delete(type: .refreshToken)
     }
 }
