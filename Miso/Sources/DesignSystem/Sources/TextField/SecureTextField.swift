fe1de8516425f1662321db277aa031adac2428d7
junseopark0331 <junseopark0331@icloud.com>
2024-01-20 23:46:46 +0900

    🗑️::[#28] 사용하지 않는 extension 삭제

--- Miso/Sources/DesignSystem/Sources/TextField/SecureTextField.swift
+++ Miso/Sources/DesignSystem/Sources/TextField/SecureTextField.swift
@@ -49,4 +49,22 @@ public final class SecureTextField: UITextField {
     }
 }
 
+extension SecureTextField: UITextFieldDelegate {
+
+//    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        if let char = string.cString(using: String.Encoding.utf8) {
+//            let isBackSpace = strcmp(char, "\\b")
+//            if isBackSpace == -92 {
+//                return true
+//            }
+//        }
+//        guard textField.text!.count < 21 else { return false }
+//        return true
+//    }
+
+    public func textFieldShouldBeginEditing(_: UITextField) -> Bool {
+        isSecureTextEntry = true
+        return true
+    }
+}
 
